57d8fc4b0b688d2632ab7951819dbb66f6cd594f
Valentin Doche <contact@valentindoche.com>
2020-11-13 15:39:44 +0100

    Expo Location + Test Code

--- App.js
+++ App.js
@@ -1,8 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+
 
 export default function App() {
+
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() =>{
+    load()
+  }, [])
+
+  async function load() {
+    try {
+      let { status } = await Location.requestPermissionsAsync()
+
+      if (status !== 'granted') {
+        setErrorMessage('Access to Location is needed to run the app')
+        return
+      }
+
+      const location = await Location.getCurrentPositionAsync()
+
+      const {latitude, longitude} = location.coords
+
+      alert(`Latitude : ${latitude}, Longitude : ${longitude}`)
+
+    } catch (error) {}
+  }
   return (
     <View style={styles.container}>
       <Text>Coucou</Text>
